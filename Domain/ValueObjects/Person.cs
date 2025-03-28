2f4e51f5c072c9595fef7f17e0486041e298ac92
Melanie <lovelystasik@gmail.com>
2025-03-28 20:43:04 +0300

    Создала модель для студента, преподавателя и некоторые вспомогательные модели

---
+++ Domain/ValueObjects/Person.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.ValueObjects;
+using Domain.ValueObjects.Base;
+
+namespace ValueObjects
+{
+    public class Person : IValueObject
+    {
+        public int Id { get; private set; }
+        public FullName Name { get; private set; }
+        public PhoneNumber PhoneNumber { get; private set; }
+        public Email Email { get; private set; }
+        public DateBirth DateOfBirth { get; private set; }
+        public List<Course> Courses { get; private set; }
+
+        public Person(int id, FullName name, PhoneNumber phoneNumber, Email email)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Courses = new List<Course>();
+        }
+
+        public FullName GetName()
+        {
+            return Name;
+        }
+
+        public PhoneNumber GetPhoneNumber()
+        {
+            return PhoneNumber;
+        }
+
+        public Email GetEmail()
+        {
+            return Email;
+        }
+
+        public DateBirth GetDateOfBirth()
+        {
+            return DateOfBirth;
+        }
+
+        public IReadOnlyList<Course> GetCourses()
+        {
+            return Courses.AsReadOnly();
+        }
+
+        public void SetCourse(Course course)
+        {
+            Courses.Add(course);
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            Courses.Remove(course);
+        }
+
+
+        public Person GetPerson()
+        {
+            return this;
+        }
+
+        public void SetPhoneNumber(PhoneNumber phoneNumber)
+        {
+            PhoneNumber = phoneNumber;
+        }
+
+        public void SetEmail(Email email)
+        {
+            Email = email;
+        }
+
+
+        public void SetDateOfBirth(DateBirth dateOfBirth)
+        {
+            DateOfBirth = dateOfBirth;
+        }
+
+    }
+}
