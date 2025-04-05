159da9ea624ffd97181100d4467da10f6f051f3e
Klrl11 <90030176+Klrl11@users.noreply.github.com>
2025-04-05 11:53:04 +0300

    feat : added classes to the DataModels

---
+++ Infrastructure/DataModels/HomeTasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataModels
+{
+    public class HomeTasks
+    {
+        public int Id { get; set; }
+        public int LessonId { get; set; }
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[] Material { get; set; }
+    }
+}
---
+++ Infrastructure/DataModels/HomeWorks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataModels
+{
+    public class HomeWorks
+    {
+        public int Id { get; set; }
+        public int HomeTaskId { get; set; }
+        public int StudentId { get; set; }
+        public int Score { get; set; }
+        public DateTime TaskCompletionDate { get; set; }
+        public byte[] Material { get; set; }
+        public string StudentComment { get; set; }
+        public string TeacherComment { get; set;}
+    }
+}
