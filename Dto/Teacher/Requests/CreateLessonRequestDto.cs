bc9f157e2333f12518c15328aaea4be30ac6a910
Kirill K <kurkin_ka@exp.gne.gov.spb.ru>
2025-04-23 22:24:49 +0300

    feat : keep doing

--- Dto/Teacher/Requests/CreateLessonRequestDto.cs
+++ Dto/Teacher/Requests/CreateLessonRequestDto.cs
@@ -1,14 +1,24 @@
 ﻿using Entities;
 using Domain.ValueObjects;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dto.Teacher.Requests
 {
     public class CreateLessonRequestDto
     {
-        public LessonName? LessonName {  get; set; } = null;
-        public string Description { get; set; } = string.Empty;
-        public DateTime Date {  get; set; }
-        public Domain.ValueObjects.File? Material { get; set; } = null;
-        public List<HomeTask> HomeTasks { get; set; } = new List<HomeTask>();
+        [Required]
+        public int TeacherId { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+        [Required]
+        public required string LessonName {  get; set; }
+        [Required]
+        public string LessonDescription { get; set; } = string.Empty;
+        [Required]
+        public DateTime LessonStartDate {  get; set; }
+        [Required]
+        public Domain.ValueObjects.File? Material { get; set; }
+        [Required]
+        public required List<HomeTask> HomeTasks { get; set; }
     }
 }
