2b2d1c78112f328a92bd22d0bc5f68a1f5d89163
Klrl11 <90030176+Klrl11@users.noreply.github.com>
2025-04-24 23:37:36 +0300

    feat: add Lesson and HomeTask factory implementations

--- Infrastructure/Factories/HomeTaskFactory.cs
+++ Infrastructure/Factories/HomeTaskFactory.cs
@@ -1,13 +1,67 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Domain.ValueObjects;
+using Infrastructure.DataModels;
 using Infrastructure.Factories.Abstractions;
 
 namespace Infrastructure.Factories
 {
-    public class HomeTaskFactory: IHomeTaskFactory
-    {
-        
+    public class HomeTaskFactory : IHomeTaskFactory
+    {        
+        public async Task<Entities.HomeTask> CreateAsync(HomeTask dataModel)
+        {
+            if (dataModel == null)
+                throw new ArgumentNullException(nameof(dataModel));
+
+            try
+            {
+                var startDate = DateOnly.FromDateTime(dataModel.StartDate);
+                var endDate = DateOnly.FromDateTime(dataModel.EndDate);
+
+                var homeTaskName = new HomeTaskName(dataModel.Title);
+                var duration = new Duration(startDate, endDate);
+                
+                Domain.ValueObjects.File? material = null;                
+
+                return new Entities.HomeTask(
+                    id: dataModel.Id,
+                    homeTaskName: homeTaskName,
+                    description: dataModel.Description,
+                    duration: duration,
+                    material: material);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(
+                    $"Ошибка при создании HomeTask из data model (ID: {dataModel.Id})", ex);
+            }
+        }
+
+        public async Task<HomeTask> CreateDataModelAsync(Entities.HomeTask domainEntity)
+        {
+            if (domainEntity == null)
+                throw new ArgumentNullException(nameof(domainEntity));
+
+            try
+            {
+                var materialPath = domainEntity.Material?.GetFullPath();
+
+                return new HomeTask
+                {
+                    Id = domainEntity.Id,
+                    //LessonId = domainEntity.LessonId,
+                    Title = domainEntity.HomeTaskName.Value,
+                    Description = domainEntity.Description,
+                    StartDate = domainEntity.Duration.StartDate.ToDateTime(TimeOnly.MinValue),
+                    EndDate = domainEntity.Duration.EndDate.ToDateTime(TimeOnly.MinValue),
+                    Material = materialPath
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(
+                    $"Ошибка при создании data model из HomeTask (ID: {domainEntity.Id})", ex);
+            }
+        }
     }
 }
