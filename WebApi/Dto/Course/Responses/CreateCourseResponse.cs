a0af32a1269e6c0f76a183bd48750599a812b9af
Kirill K <kurkin_ka@exp.gne.gov.spb.ru>
2025-05-13 13:34:27 +0300

    fix

---
+++ WebApi/Dto/Course/Responses/CreateCourseResponse.cs
@@ -0,0 +1,13 @@
+namespace WebApi.Dto.Course.Responses;
+
+public class CreateCourseResponse
+{
+        public int Id { get; set; }
+        public int StateId {  get; set; }
+        public int TeacherId { get; set; }
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set;}
+        public int PassingScore { get; set; }
+}
--- WebApi/Dto/Teacher/Requests/CreateLessonRequestDto.cs
+++ WebApi/Dto/Teacher/Requests/CreateLessonRequestDto.cs
@@ -5,50 +5,18 @@ using File = Domain.ValueObjects.File;
 
 namespace WebApi.Dto.Teacher.Requests
 {
-
-    /// <summary>
-    /// Модель для создания занятия
-    /// </summary>
     public class CreateLessonRequestDto
     {
-        /// <summary>
-        /// Id преподавателя, создающего занятие
-        /// </summary>
-        [Required(ErrorMessage = "Id преподавателя обязателен")]
-        [Range(1, long.MaxValue, ErrorMessage = "Id преподавателя должен быть положительным числом")]
+        [Required]
         public long TeacherId { get; set; }
-
-        /// <summary>
-        /// Id курса, которому будет принадлежать занятие
-        /// </summary>
-        [Required(ErrorMessage = "Id курса обязателен")]
-        [Range(1, long.MaxValue, ErrorMessage = "Id курса должен быть положительным числом")]
+        [Required]
         public long CourseId { get; set; }
-
-        /// <summary>
-        /// Название занятия
-        /// </summary>
-        [Required(ErrorMessage = "Название занятия обязателено")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "Длина названия должна быть от 2 до 100 символов.")]
-        [RegularExpression(@"^[\p{L}\d\s\-,.:;!?()_№]+$",
-        ErrorMessage = "Название может содержать только буквы, цифры, пробелы и ,.-:;!?()_№")]
+        [Required]
         public string LessonName { get; set; }
-
-        /// <summary>
-        /// Описание занятия
-        /// </summary>
-        [Required(ErrorMessage = "Описание занятия обязателено")]
+        [Required]
         public string LessonDescription { get; set; }
-
-        /// <summary>
-        /// Дата проведения занятия с точностью до минуты
-        /// </summary>
-        [Required(ErrorMessage = "Дата проведения занятия обязателена")]
+        [Required]
         public DateTime LessonStartDate { get; set; }
-
-        /// <summary>
-        /// Учебные материалы, прилагаемые к занятию
-        /// </summary>
         public File? Material { get; set; }
     }
 }
