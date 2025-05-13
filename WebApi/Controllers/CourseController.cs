b63ffcc07f54ea97e3258fa84b87ba1accb1afc5
Kirill K <kurkin_ka@exp.gne.gov.spb.ru>
2025-05-13 13:30:54 +0300

    fix

---
+++ WebApi/Controllers/CourseController.cs
@@ -0,0 +1,65 @@
+using Application.Models.Course;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Dto.Course.Requests;
+using WebApi.Dto.Course.Responses;
+
+namespace WebApi.Controllers;
+
+/// <summary>
+/// Контроллер курса
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class CourseController : ControllerBase
+{
+    public CourseController(ICourseService courseService)
+    {
+        _courseService = courseService;
+    }
+
+    /// <summary>
+    /// Метод создание курса
+    /// </summary>
+    /// <param name="courseData"></param>
+    /// <returns></returns>
+    /// <response code="200">Возвращает курс</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateCourse([FromBody] CreateCourseRequest courseData)
+    {
+        try
+        {
+            var createdCourse = await _courseService.AddCourseAsync(new CreateCourseModel()
+            {
+                StateId = courseData.StateId,
+                TeacherId = courseData.TeacherId,
+                Title = courseData.Title,
+                Description = courseData.Description,
+                StartDate = courseData.StartDate,
+                EndDate = courseData.EndDate,
+                PassingScore = courseData.PassingScore
+            });
+
+            var createdCourseResponse = new CreateCourseResponse()
+            {
+                Id = createdCourse.Id,
+                StateId = (int)createdCourse.StateId,
+                TeacherId = createdCourse.TeacherId,
+                Title = createdCourse.Title,
+                Description = createdCourse.Description,
+                StartDate = createdCourse.StartDate,
+                EndDate = createdCourse.EndDate,
+                PassingScore = createdCourse.PassingScore
+            };
+            return Ok(createdCourseResponse);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Внутренняя ошибка сервера");
+        }
+    }
+
+    private ICourseService _courseService;
+}
--- WebApi/Controllers/TeacherLessonController.cs
+++ WebApi/Controllers/TeacherLessonController.cs
@@ -9,86 +9,17 @@ using File = Domain.ValueObjects.File;
 
 namespace WebApi.Controllers
 {
-    /// <summary>
-    /// Контроллер для взаимодействия преподавателя с занятием
-    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
-    [Produces("application/json")]
     public class TeacherLessonController : ControllerBase
     {
         private readonly ITeacherLessonService _teacherService;
 
-        /// <summary>
-        /// Конструктор
-        /// </summary>
-        /// <param name="teacherService"></param>
         public TeacherLessonController(ITeacherLessonService teacherService)
         {
             _teacherService = teacherService;
         }
-
-        /// <summary>
-        /// Метод создания занятия
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns>Модель созданного в БД пользователя</returns>
-        /// <remarks>
-        /// Пример запроса:
-        ///
-        ///     POST /create-lesson
-        ///     {
-        ///        "teacherId": 111,
-        ///        "courseId": 222,
-        ///        "lessonName": "Название занятия",
-        ///        "lessonDescription": "Описание занятия",
-        ///        "lessonStartDate": "2025-05-12T20:00:01.783Z",
-        ///        "material": {
-        ///              "path": "C/Prog",
-        ///              "extension": "txt",
-        ///              "name": "FileName"
-        ///              }
-        ///     }
-        ///
-        /// </remarks>
-        /// <response code="201">Возвращает id созданного занятия</response>
-        /// <response code="500">Если есть какие-то ошибки при создании</response>        
-        [HttpPost("create-lesson")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreateLesson(CreateLessonRequestDto request)
-        {
-            try
-            {
-                if (request.LessonStartDate <= DateTime.Now)
-                {
-                    ModelState.AddModelError(nameof(request.LessonStartDate),
-                        "Дата занятия должна быть в будущем");
-                    return BadRequest(ModelState);
-                }
-
-                LessonName lessonName = new LessonName(request.LessonName);
-
-                var requestModel = new CreateLessonModel
-                {
-                    TeacherId = (int)request.TeacherId,
-                    CourseId = (int)request.CourseId,
-                    LessonName = lessonName,
-                    LessonDescription = request.LessonDescription,
-                    LessonStartDate = request.LessonStartDate,
-                    Material = request.Material,
-                };
-
-                var result = await _teacherService.CreateLesson(requestModel);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                // Логирование ошибки (можно добавить _logger)
-                return StatusCode(500, $"Internal server error: {ex}");
-            }            
-        }
-        //[HttpGet("calendar-data")]
+        [HttpGet("calendar-data")]
         //public async Task<IActionResult> GetCalendarData(GetCalendarDataRequestDto requestDto)
         //{
         //    if (requestDto is null
@@ -100,5 +31,30 @@ namespace WebApi.Controllers
         //    var result = await _teacherService.GetCalendarData(requestDto);
         //    return Ok(result);
         //}
+        [HttpPost("create-lesson")]
+        public async Task<IActionResult> CreateLesson(CreateLessonRequestDto requestDto)
+        {           
+            try
+            {   LessonName lessonName = new LessonName(requestDto.LessonName);
+
+                var requestModel = new CreateLessonModel
+                {
+                    TeacherId = (int)requestDto.TeacherId,
+                    CourseId = (int)requestDto.CourseId,
+                    LessonName = lessonName,
+                    LessonDescription = requestDto.LessonDescription,
+                    LessonStartDate = requestDto.LessonStartDate,
+                    Material = requestDto.Material,
+                };
+
+                var result = await _teacherService.CreateLesson(requestModel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки (можно добавить _logger)
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
     }
 }
