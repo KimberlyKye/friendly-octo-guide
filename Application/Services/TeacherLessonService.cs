e11da992a7496819cf628f93560ddd7d6f577c5c
Klrl11 <90030176+Klrl11@users.noreply.github.com>
2025-05-31 21:02:57 +0300

    feat : add endpoint for Student's Calendar fix ; remove Exeptions from Fabrics

--- Application/Services/TeacherLessonService.cs
+++ Application/Services/TeacherLessonService.cs
@@ -22,24 +22,30 @@ public class TeacherLessonService : ITeacherLessonService
         _courseInfoRepository = courseInfoRepository;
         _teacherInfoRepository = teacherInfoRepository;
     }
-    //public async Task<CalendarResponseModel> GetCalendarData(GetCalendarDataRequestModel requestDto)
-    //{
-    //    return await _teacherRepository.GetCalendarData(requestDto);
-    //}
-    public async Task<int> CreateLesson(CreateLessonModel requestDto)
+    
+    public async Task<int> CreateLesson(CreateLessonModel request)
     {
-        Teacher teacher = await _teacherInfoRepository.GetTeacherById(requestDto.TeacherId);
-
-        if (!await _courseInfoRepository.CheckIsCourseExistAndActiveById(requestDto.CourseId))
-        { return -1; }
+        Teacher? teacher = await _teacherInfoRepository.GetTeacherById(request.TeacherId);
+        if (teacher is null) 
+        {
+            throw new ArgumentNullException($"Преподаватель с ID {request.TeacherId} не существует", nameof(request.TeacherId));
+        }
+        if (!await _courseInfoRepository.CheckIsCourseExistAndActiveById(request.CourseId))
+        {
+            throw new ArgumentNullException($"Курс с ID {request.CourseId} не существует или уже в архиве", nameof(request.CourseId));
+        }
+        if (!await _courseInfoRepository.IsCourseOwnedByTeacherAsync(request.CourseId, teacher.Id))
+        {
+            throw new ArgumentNullException($"Курс с ID {request.CourseId} не не принадлежит преподавателю с ID {request.TeacherId}", nameof(request.CourseId));
+        }
 
         Lesson newLesson = await teacher.CreateLesson(  0,
-                                                        requestDto.CourseId,
-                                                        requestDto.LessonName,
-                                                        requestDto.LessonDescription,
-                                                        requestDto.LessonStartDate,
-                                                        requestDto.Material,
-                                                        requestDto.HomeTasks);
+                                                        request.CourseId,
+                                                        request.LessonName,
+                                                        request.LessonDescription,
+                                                        request.LessonStartDate,
+                                                        request.Material,
+                                                        request.HomeTasks);
 
         return await _teacherLessonRepository.AddLesson(newLesson);
     }    
