714329188d0db3b6a43fb8404b148032d8dc6be1
Klrl11 <90030176+Klrl11@users.noreply.github.com>
2025-06-06 23:29:44 +0300

    feat : add some code

---
+++ Infrastructure/Repositories/StudentInfoRepository.cs
@@ -0,0 +1,62 @@
+﻿using Domain.ValueObjects.Enums;
+using Entities;
+using Infrastructure.Contexts;
+using Infrastructure.Factories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using RepositoriesAbstractions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class StudentInfoRepository : IStudentInfoRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly IStudentFactory _studentFactory;
+        private readonly ICourseFactory _courseFactory;
+
+
+        public StudentInfoRepository(
+            AppDbContext context,
+            IStudentFactory studentFactory,
+            ICourseFactory courseFactory)
+        {
+            _context = context;
+            _studentFactory = studentFactory;
+            _courseFactory = courseFactory;
+        }
+        
+        public async Task<Student?> GetStudentById(int studentId)
+        {
+
+            var studentInfo = await _context.Users
+                .Where(student => student.Id == studentId && student.RoleId == (int)RoleEnum.Student)
+                .FirstOrDefaultAsync();
+
+            if (studentInfo is null) { return null; }
+
+            return await _studentFactory.CreateFromAsync(studentInfo);
+        }
+        public async Task<List<Course>> GetAllCourses(int studentId)
+        {
+            var coursesData = await(
+                from student in _context.Users.AsNoTracking()
+                from studentCourse in _context.StudentCourses
+                    .Where(sc => sc.StudentId == student.Id)
+                from course in _context.Courses
+                    .Where(c => c.Id == studentCourse.CourseId)
+                where student.Id == studentId
+                    && student.RoleId == (int)RoleEnum.Student
+                select new { Course = course, Teacher = course.Teacher })
+                .ToListAsync();
+
+            var result = _courseFactory.
+
+            return coursesData;
+        }
+
+    }
+}
---
+++ RepositoriesAbstractions/Abstractions/IStudentInfoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoriesAbstractions.Abstractions
+{
+    public interface IStudentInfoRepository
+    {
+        Task<Student?> GetStudentById(int studentId);
+        Task<List<Course>> GetAllCourses(int studentId);
+    }
+}
