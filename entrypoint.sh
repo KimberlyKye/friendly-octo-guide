549d497b390b478b9abdc9e86ae6f7c3d77c4d12
Melanie <54101611+KimberlyKye@users.noreply.github.com>
2025-08-01 00:03:51 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Применяем миграции (Render сам подставит переменные окружения)
-dotnet ef database update --no-build
+# Применяем миграции через ef (теперь он доступен)
+dotnet ef database update --no-build --startup-project /app/WebApi.dll --project /app/WebApi.dll
 
 # Запускаем приложение
 exec dotnet WebApi.dll
