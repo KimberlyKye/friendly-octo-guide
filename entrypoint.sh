895555efb5e76ae2f9011881f2dbbfa5371b2251
Melanie <54101611+KimberlyKye@users.noreply.github.com>
2025-07-31 23:56:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
 
-# Ждем доступности БД (теперь с таймаутом)
-for i in {1..30}; do
-  if </dev/tcp/$DB_HOST/$DB_PORT; then
-    echo "Database is ready!"
-    break
-  fi
-  echo "Waiting for database... ($i/30)"
-  sleep 2
-done
-
-# Применяем миграции
+# Применяем миграции (Render сам подставит переменные окружения)
 dotnet ef database update --no-build
 
 # Запускаем приложение
